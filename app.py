e01c8a800af7b6ee6eacd1cce7ac426521964333
sanaa-repo <sanaakapur@gmail.com>
2023-05-09 11:26:20 -0700

    Working TimeMatch wo. new aesthetic

--- app.py
+++ app.py
@@ -4,25 +4,148 @@ from models import *
 
 app = Flask(__name__)
 
-# TODO: Change the secret key
-app.secret_key = "Change Me"
+app.secret_key = "tZ350KsDn78"
 
-# TODO: Fill in methods and routes
+# TODO: add comments
 
-@app.route("/")
-def home():
-    return render_template("login.html")
+@app.route("/", methods = ["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        myUser = db_session.query(User).where((User.username == username) & (User.password == password)).first()
+        if myUser is not None and myUser.username == username and myUser.password == password:
+            session["username"] = username 
+            return redirect(url_for("myprofile"))
+        else:
+            flash("Incorrect username or password. Try again", "error")
+            return redirect(url_for("login"))
 
-@app.route("/signup")
+#Create an account with Username, Password, Email, and Mentor/Student
+@app.route("/signup", methods = ["GET", "POST"])
 def signup():
-    return render_template("signup.html")
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        email = request.form["email"]
+        usertype = request.form["user-type"]
+        usernameExists = db_session.query(User).where(User.username == username).first()
+        #only create new user if username isn't already taken
+        if usernameExists is None:
+            db_session.add(User(username, password, email, usertype))
+            db_session.commit()
+            return redirect(url_for("login"))
+        else:
+            flash("Username already exists. Please create another one", "error")
+            return redirect(url_for("signup"))
 
+#
+def match():
+    myUser = db_session.query(User).where(User.username == session["username"]).first()
+    finalList = []
+    mentorList = db_session.query(User).where(User.usertype == "mentor").all()
+    studentList = db_session.query(User).where(User.usertype == "student").all()
+    if(myUser.usertype == "student"):
+        for mentor in mentorList:
+            for time in mentor.times:
+                if(time in myUser.times):
+                    for subject in mentor.subjects:
+                        if(subject in myUser.subjects):
+                            appendString = mentor.username + "," + str(subject) + "," + str(time) + "," + mentor.email
+                            finalList.append(appendString)
+    elif(myUser.usertype == "mentor"):
+        for student in studentList:
+            for time in student.times:
+                if(time in myUser.times):
+                    for subject in student.subjects:
+                        if(subject in myUser.subjects):
+                            appendString = student.username + "," + str(subject) + "," + str(time) + "," + student.email
+                            finalList.append(appendString)
+    return finalList
+
+
+#Displays a users subjects and availabilities as well as the results of the time matching
 @app.route("/myprofile")
 def myprofile():
-    return render_template("myprofile.html")
-@app.route("/addinfo")
+    myUser = db_session.query(User).where(User.username == session.get("username")).first()
+    usrnm = myUser.username
+    subjects = myUser.subjects
+    timelist = myUser.times
+    userList = match()
+    splitList = []
+    for string in userList:
+        splitList.append(string.split(","))
+    return render_template("myprofile.html", username = usrnm, subjectlist = subjects, timelist = timelist, userlist = splitList)
+
+#can add/delete subjects and times
+@app.route("/addinfo", methods = ["GET", "POST"])
 def addinfo():
-    return render_template("myinfo.html")
+    if request.method == "GET":
+        return render_template("myinfo.html")
+    elif request.method == "POST":
+        if "subject_submit" in request.form:
+            subject = request.form["subject"]
+            myUserID = db_session.query(User.id).where(User.username == session["username"]).first()[0]
+            subjectExist = db_session.query(Subject.name).where(Subject.name == subject).first()
+            #add the subject if it doesn't exist, otherwise just link the user and subject
+            if subjectExist is None:
+                db_session.add(Subject(subject))
+                db_session.commit()
+            currSubjectID = db_session.query(Subject.id).where(Subject.name == subject).first()[0]
+            db_session.add(TutorSubject(currSubjectID, myUserID))
+            db_session.commit()
+            return redirect(url_for("myprofile"))
+        elif "subject_delete_submit" in request.form:
+            subject = request.form["delete-subject"] 
+            subjectDeleteID = db_session.query(Subject.id).where(Subject.name == subject).first()[0]
+            myUserID = db_session.query(User.id).where(User.username == session["username"]).first()[0]
+            if subjectDeleteID is None:
+                flash("This subject was not added", "info")
+                return redirect(url_for("addinfo"))
+            TutorSubjectToDelete = db_session.query(TutorSubject).where((TutorSubject.subject_id == subjectDeleteID) & (TutorSubject.user_id == myUserID)).first()
+            db_session.delete(TutorSubjectToDelete)
+            db_session.commit()
+            return redirect(url_for("myprofile"))
+        elif "time_submit" in request.form:
+            timeString = request.form["add-time"]
+            timelist = timeString.split(" ")
+            day_of_week = timelist[0]
+            time = timelist[1]
+            myUserID = db_session.query(User.id).where(User.username == session["username"]).first()[0]
+            timeExist = db_session.query(Time).where((Time.day_of_week == day_of_week) & (Time.time == time)).first()
+            if timeExist is None:
+                db_session.add(Time(day_of_week, time))
+                db_session.commit()
+            currTimeID = db_session.query(Time.id).where((Time.day_of_week == day_of_week) & (Time.time == time)).first()[0]
+            db_session.add(Availability(myUserID, currTimeID))
+            db_session.commit()
+            return redirect(url_for("myprofile"))
+        elif "time_delete_submit" in request.form:
+            timeString = request.form["delete-time"]
+            timelist = timeString.split(" ")
+            day_of_week = timelist[0]
+            time = timelist[1]
+            myUserID = db_session.query(User.id).where(User.username == session["username"]).first()[0]
+            timeDeleteID = db_session.query(Time.id).where((Time.day_of_week == day_of_week) & (Time.time == time)).first()[0]
+            if timeDeleteID is None:
+                flash("This time was not added", "info")
+                return redirect(url_for("addinfo"))
+            AvailabilityToDelete = db_session.query(Availability).where((Availability.time_id == timeDeleteID) & (Availability.user_id == myUserID)).first()
+            db_session.delete(AvailabilityToDelete)
+            db_session.commit()
+            return redirect(url_for("myprofile"))
+
+
+@app.route("/logout")
+def logout():
+    if "username" in session:
+        session.pop("username")  
+        flash("You've been logged out", "info")
+    return redirect(url_for("login"))
 
 
 @app.before_first_request
---
+++ models.py
@@ -0,0 +1,109 @@
+"""
+The file that holds the schema/classes
+that will be used to create objects
+and connect to data tables.
+"""
+
+from sqlalchemy import ForeignKey, Column, INTEGER, TEXT
+from sqlalchemy.orm import relationship
+from database import Base
+
+# TODO: Complete your models
+
+class User(Base):
+    __tablename__ = "users"
+
+    # Columns
+    id = Column("id", INTEGER, autoincrement = True, primary_key=True)
+    username = Column("username", TEXT, nullable=False)
+    password = Column("password", TEXT, nullable=False)
+    email = Column("email", TEXT, nullable=False)
+    usertype = Column("usertype", TEXT, nullable=False)
+    availabilities = relationship("Availability", back_populates="user")
+    subjects = relationship("Subject", secondary = "tutorsubjects", back_populates = "users")
+    times = relationship("Time", secondary = "availabilities", back_populates = "users")
+
+    #relationships: None for now. The user is forever alone:)
+
+    #constructor
+    def __init__(self, username, password, email, usertype):
+        # id auto-increments
+        self.username = username
+        self.password = password
+        self.email = email
+        self.usertype = usertype
+
+    def __repr__(self):
+        return self.username + " is a " + self.usertype + ". Their email is" + self.email
+
+class TutorSubject(Base):
+    __tablename__ = "tutorsubjects"
+
+    #Columns
+    id = Column("id", INTEGER, autoincrement = True, primary_key=True)
+    subject_id = Column("subject_id", ForeignKey("subjects.id"))
+    user_id = Column("user_id", ForeignKey("users.id"))
+
+    def __init__(self, subject_id, user_id):
+        # id auto-increments
+        self.subject_id = subject_id
+        self.user_id = user_id
+
+
+class Subject(Base):
+    __tablename__ = "subjects"
+
+    #Columns
+    id = Column("id", INTEGER, autoincrement = True, primary_key=True)
+    name = Column("name", TEXT, nullable = False)
+    users = relationship("User", secondary = "tutorsubjects", back_populates = "subjects")
+    #constructor
+    def __init__(self, name):
+        #id auto-increments
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+    
+class Availability(Base):
+    __tablename__ = "availabilities"
+
+    #Columns
+    id = Column("id", INTEGER, autoincrement = True, primary_key=True)
+    user_id = Column("user_id", ForeignKey("users.id"))
+    time_id = Column("time_id", ForeignKey("times.id"))
+    time = relationship("Time", back_populates= "availabilities")
+    user = relationship("User", back_populates= "availabilities")
+    
+    #constructor
+    def __init__(self, user_id, time_id):
+        #id autoincrements
+        self.user_id = user_id
+        self.time_id = time_id
+    
+class Time(Base):
+    __tablename__ = "times"
+
+    #Columns 
+    id = Column("id", INTEGER, autoincrement = True, primary_key=True)
+    day_of_week = Column("day_of_week", TEXT, nullable = False)
+    time = Column("time", INTEGER, nullable = False)
+    availabilities = relationship("Availability", back_populates="time") 
+    users = relationship("User", secondary = "availabilities", back_populates = "times")
+
+
+    def __init__(self, day_of_week, time):
+        #id autoincrements
+        self.day_of_week = day_of_week
+        self.time = time
+
+    def __repr__(self):
+        return self.day_of_week + " @ " + str(self.time)
+
+
+
+
+    
+
+
+    
